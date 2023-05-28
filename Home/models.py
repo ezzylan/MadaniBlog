df1da087650d31dad0332d5e5141fe3d3a301349
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2023-05-28 15:37:05 +0800

    edited models.py

--- Home/models.py
+++ Home/models.py
@@ -1,18 +1,17 @@
-from django.db import models
-from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
-from django.contrib.auth.models import User
 from datetime import datetime
+from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to=None, null=True, default='null')
-    video = models.FileField(upload_to=None, null=True, default='null')
+    image = models.ImageField(upload_to=None, null=True, default="null")
+    video = models.FileField(upload_to=None, null=True, default="null")
     creation_datetime = models.DateTimeField(auto_now=True)
-    modification_datetime = models.DateTimeField(
-        default=datetime.now, blank=True)
+    modification_datetime = models.DateTimeField(default=datetime.now, blank=True)
 
     def save(self, *args, **kwargs):
         self.modification_datetime = datetime.now()
@@ -29,10 +28,28 @@ class Comment(models.Model):
 
 class Blogger(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birthdate = models.DateField()
-    profile_image = models.ImageField(upload_to=None)
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_image = models.ImageField(upload_to="profile_images/")
+
+    # social links
+    twitter_link = models.URLField(blank=True)
+    instagram_link = models.URLField(blank=True)
+    facebook_link = models.URLField(blank=True)
+    website_link = models.URLField(blank=True)
+
+    # blog info
+    blog_title = models.CharField(max_length=500)
+    blog_description = models.TextField(max_length=500, blank=True)
+    blog_image = models.ImageField(upload_to="blog_images/")
+
     following_users = models.ManyToManyField(
-        User, related_name="following_user", default='null')
-    follower = models.ManyToManyField(
-        User, related_name='follower', default='null')
-    fav_post = models.ManyToManyField(Post, default='null')
+        User, related_name="following_user", default="null"
+    )
+    follower = models.ManyToManyField(User, related_name="follower", default="null")
+    fav_post = models.ManyToManyField(Post, default="null")
+    slug = models.SlugField(max_length=150, default='null')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.user.username)
+        return super().save(*args, **kwargs)
