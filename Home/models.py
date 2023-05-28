a79159bbd2a2c978677e1a8be467421bd9c973b0
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2023-05-28 20:36:38 +0800

    updated models.py

--- Home/models.py
+++ Home/models.py
@@ -1,15 +1,25 @@
 from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
-from django.template.defaultfilters import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.templatetags.static import static
+from autoslug import AutoSlugField
+
+
+def user_directory_path(instance, filename):
+    if isinstance(instance, Post):
+        return "user_{0}/posts/{1}".format(instance.user.id, filename)
+    elif isinstance(instance, Blogger):
+        return "user_{0}/bloggers/{1}".format(instance.user.id, filename)
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to=None, null=True, default="null")
-    video = models.FileField(upload_to=None, null=True, default="null")
+    image = models.ImageField(upload_to=user_directory_path, null=True, default="null")
+    video = models.FileField(upload_to=user_directory_path, null=True, default="null")
     creation_datetime = models.DateTimeField(auto_now=True)
     modification_datetime = models.DateTimeField(default=datetime.now, blank=True)
 
@@ -18,7 +28,8 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.title;
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -28,9 +39,12 @@ class Comment(models.Model):
 
 class Blogger(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    profile_image = models.ImageField(upload_to="profile_images/")
+    profile_image = models.ImageField(
+        upload_to=user_directory_path,
+        default=static("images/placeholder-profile-icon.jpg"),
+    )
+    tagline = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
 
     # social links
     twitter_link = models.URLField(blank=True)
@@ -40,16 +54,23 @@ class Blogger(models.Model):
 
     # blog info
     blog_title = models.CharField(max_length=500)
-    blog_description = models.TextField(max_length=500, blank=True)
-    blog_image = models.ImageField(upload_to="blog_images/")
+    blog_description = models.TextField(blank=True)
+    blog_image = models.ImageField(upload_to=user_directory_path, blank=True)
 
     following_users = models.ManyToManyField(
         User, related_name="following_user", default="null"
     )
     follower = models.ManyToManyField(User, related_name="follower", default="null")
     fav_post = models.ManyToManyField(Post, default="null")
-    slug = models.SlugField(max_length=150, default='null')
+    slug = AutoSlugField(populate_from="user")
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.user.username)
-        return super().save(*args, **kwargs)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Blogger.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
