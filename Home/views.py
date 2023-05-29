7c8bbe4981a90a2f38489f8164113f75887bfca7
Ma Li <3377320340mali@gmail.com>
2023-05-29 23:57:20 +0800

    home model upgrade

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Post
 
-# Create your views here.
-def home(request):
-    return render (request,'home.html')
+class Home(TemplateView):
+    template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_blogs'] = Post.objects.all().order_by('-creation_datetime')[:5]
+        
+        return context
