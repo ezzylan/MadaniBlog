1fff92d9d2d1ea5419dcc573fa540051b4337288
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2023-05-30 00:24:33 +0800

    added profile url

--- MadaniBlog/urls.py
+++ MadaniBlog/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('Home.urls', 'Home'), namespace='Home')),
-    # path('profile/', include(('Profile.urls', 'Profile'), namespace='Profile')),
-]
+    path("admin/", admin.site.urls),
+    path("", include(("Home.urls", "Home"), namespace="Home")),
+    path("profile/", include(("Profile.urls", "Profile"), namespace="Profile")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
